2f022a24102d50bfe49d74bb5d78027afb4e69f7
AliciaD <alicia09diazv@gmail.com>
2023-10-03 22:48:07 -0600

    Se visualizó el codigo que nos dejaron de tarea, hicimos una conexiones y arreglamos JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog'); //Se agrega un . porque es una clase
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) { //se agrega un async
+  $n.textContent = 'cargando...';  
+  const response = await fetch(`${usersEndpoint}/${username}`); //No puede existir un await sin un async, asi que la agregamos 
+  const data = await response.json(); //Se agrega JSON con un await
+
+  //Recibe informacion en formato JSON
+  console.log(data); 
+  $n.textContent = data.name; //Lo convierte a string
+
+  //console.log($n);
+  $b.textContent = data.blog;
+  $l.textContent = data.location; //No esta en html, hay que agregarlo 
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` //Se acomoda la variable con el $
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); //Manda llamar la funcion, le mete el usuario 
+
+/*El codigo se conecta con una API y asi recibir un objeto JSON para poder mostrarlo en consola. Además proporciona manejo de errores básico para tratar problemas de conexión o datos incorrectos. */
